8f19962e543f7934e10c9f3e3a8bd63c7d722723
Leah Doskoez <ldoskoez@aol.com>
2020-12-06 23:31:10 -0600

    Implementation of stacks for the lists in the table view with proper spacing and positioning with no styling.

--- FindAPet/SearchViewController.swift
+++ FindAPet/SearchViewController.swift
@@ -8,20 +8,33 @@
 import UIKit
 
 class PetTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var petName: UILabel!
-    @IBOutlet weak var petAge: UILabel!
     @IBOutlet weak var petPic: UIImageView!
+    @IBOutlet weak var petAge: UILabel!
+    @IBOutlet weak var petName: UILabel!
+    
     
 }
 
 class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
+    
+    //struct definitions
+    struct AnimalsResponse : Codable{
+        let animals: [Animal]?
+    }
+    
+    struct Animal: Codable {
+        let id: Int?
+        let type: String?
+        let name: String?
+        let gender: String?
+    }
+    
+    
     //declarations
     var finalZipcode = ""
     @IBOutlet weak var ZipcodeLabel: UILabel!
     @IBOutlet weak var SearchTable: UITableView!
-    var fetchedPosts = [Post]()
     var fetchedAnimals : [Animal]!
     
     //Loads the zipcode search
@@ -30,47 +43,13 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         SearchTable.delegate = self
         SearchTable.dataSource = self
-        
         ZipcodeLabel.text = "Searching for pets in the zipcode: \(finalZipcode)"
         
         //fetchPostData(completionHandler: processPostData(posts:))
+        //Fetches data from json file to parse into readable animals for adoption
         fetchedAnimals = parse(jsonData: readLocalFile(forName: "pets_60614")!)?.animals
     }
     
-    func processPostData (posts : [Post] ){
-        DispatchQueue.main.async {
-            for posts in posts{
-                print(posts.title!)
-            }
-            self.fetchedPosts = posts
-            self.SearchTable.reloadData()
-        }
-    }
-    
-    //fetch post data and compiles it to data that we can use
-    func fetchPostData(completionHandler : @escaping ([Post]) -> Void){
-        
-        
-        let url = URL(string : "https://jsonplaceholder.typicode.com/posts")!
-        
-        URLSession.shared.dataTask(with: url) {
-            (data, response, error) in
-            guard let data = data
-            else{
-                return
-            }
-            
-            do{
-                let postsData = try JSONDecoder().decode([Post].self, from: data)
-                completionHandler(postsData)
-            }
-            
-            catch{
-                let error = error
-                print(error.localizedDescription)
-            }
-        }.resume()
-    }
     
     private func readLocalFile(forName name: String) -> Data? {
         do {
@@ -91,8 +70,8 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
             let animalsResponse = try JSONDecoder().decode(AnimalsResponse.self,
             from: jsonData) as AnimalsResponse
             
-            print("Animal ID: ", animalsResponse.animals?[0].id ?? "None")
-            print("Animal Name:  ", animalsResponse.animals?[0].name ?? "None")
+//            print("Animal ID: ", animalsResponse.animals?[0].id ?? "None")
+//            print("Animal Name:  ", animalsResponse.animals?[0].name ?? "None")
             
             return animalsResponse
         } catch {
@@ -108,34 +87,14 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PetTableViewCell
-
         let currentAnimal = fetchedAnimals[indexPath.row]
         cell.petName?.text = currentAnimal.name
         cell.petAge?.text = "\(currentAnimal.gender ?? "") \(currentAnimal.type ?? "")"
+//        cell.petPic?.image = UIImage(named: "catdog")
       
 //        cell.contentConfiguration = content
 
         return cell
-    }
-    
-    
-    //struct and class definitions
-    struct AnimalsResponse : Codable{
-        let animals: [Animal]?
-    }
-    
-    struct Animal: Codable {
-        let id: Int?
-        let type: String?
-        let name: String?
-        let gender: String?
-    }
-
-    class Post : Codable{
-        var userId : Int!
-        var id : Int!
-        var title : String!
-        var body : String!
     }
     
     
